a75fd18691cddec1c83713d75f62b455b6d348a8
Christof Duhme <christof.duhme@gmail.com>
2023-08-19 00:15:45 +0200

    Add nvim.

---
+++ .config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,32 @@
+local plugins = {
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = {"python"},
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "black",
+        "mypy",
+        "pyright",
+        "ruff",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+}
+return plugins
