20407668d68963d0fd9c8e10aa73d7d52a1dcd38
Christof Duhme <christof.duhme@gmail.com>
2025-05-17 20:31:58 +0200

    Update.

---
+++ .config/nvim/lua/custom/configs/vimspector.lua
@@ -0,0 +1,22 @@
+vim.g.vimspector_enable_mappings = "HUMAN"
+local vimspector = {
+  'puremourning/vimspector',
+  keys = { '<F1>', '<F5>' },
+}
+vimspector.config = function()
+  vim.keymap.set('n', '<F1>', '<Plug>VimspectorToggleBreakpoint')
+  vim.keymap.set('n', '<F2>', '<Plug>VimspectorToggleConditionalBreakpoint')
+  vim.keymap.set('n', '<F3>', '<Plug>VimspectorAddFunctionBreakpoint')
+  vim.keymap.set('n', '<F4>', '<Plug>VimspectorRunToCursor')
+  vim.keymap.set('n', '<F5>', '<Plug>VimspectorContinue')
+  vim.keymap.set('n', '<Right>', '<Plug>VimspectorStepOver')
+  vim.keymap.set('n', '<Up>', '<Plug>VimspectorStepOut')
+  vim.keymap.set('n', '<Down>', '<Plug>VimspectorStepInto')
+
+  vim.api.nvim_create_user_command('VimspectorPause', 'vimspector#Pause()', { force = true })
+  vim.api.nvim_create_user_command('VimspectorStop', 'vimspector#Stop()', { force = true })
+
+  return vimspector
+end
+
+return vimspector
