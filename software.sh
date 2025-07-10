b93380252454aa12e46f479fbb9d79e05f67961c
Christof Duhme <christof.duhme@gmail.com>
2025-07-10 07:01:19 -0100

    Add software install script.

---
+++ software.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+sudo pacman -S --noconfirm --needed alacritty zsh tmux fzf jq bitwarden-cli waybar hyprlock brightnessctl
+sudo pacman -S --noconfirm --needed xdg-desktop-portal-gtk gsettings thunar thunar-volman gvfs
+sudo pacman -S --noconfirm --needed ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols ttf-font-awesome
