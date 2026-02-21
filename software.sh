06db5fa077e82c392125f22f42fc5cef440aa03d
Christof Duhme <christof.duhme@gmail.com>
2026-02-21 14:42:48 -0100

    Add starship and micromamba.

--- software.sh
+++ software.sh
@@ -1,7 +1,13 @@
 #!/bin/bash 
 
-sudo pacman -S --noconfirm --needed alacritty zsh tmux nvim tree-sitter-cli less zoxide fzf jq direnv
+sudo pacman -S --noconfirm --needed alacritty zsh starship tmux nvim tree-sitter-cli less zoxide fzf jq direnv
 sudo pacman -S --noconfirm --needed bitwarden-cli
 sudo pacman -S --noconfirm --needed waybar rofi hyprlock brightnessctl
 sudo pacman -S --noconfirm --needed xdg-desktop-portal-gtk thunar thunar-volman gvfs
 sudo pacman -S --noconfirm --needed ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols ttf-font-awesome
+
+if [ -f "$HOME/.local/bin/micromamba" ]; then
+    echo "micromamba is already installed"
+else
+    curl -sSL micro.mamba.pm/install.sh | sh
+fi
