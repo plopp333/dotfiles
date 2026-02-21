2d8d65ed11aae1355094435d5e1287f1b272ab67
Christof Duhme <christof.duhme@gmail.com>
2026-02-21 14:21:29 -0100

    Fix typo.

--- software.sh
+++ software.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 sudo pacman -S --noconfirm --needed alacritty zsh tmux nvim tree-sitter-cli less zoxide fzf jq direnv
-sudo pacman -S --noconfirm --needed bitwarden-clt
+sudo pacman -S --noconfirm --needed bitwarden-cli
 sudo pacman -S --noconfirm --needed waybar rofi hyprlock brightnessctl
 sudo pacman -S --noconfirm --needed xdg-desktop-portal-gtk thunar thunar-volman gvfs
 sudo pacman -S --noconfirm --needed ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols ttf-font-awesome
