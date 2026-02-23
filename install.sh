48195f37be37068f88bd935f6ba2c0f264178e98
Christof Duhme <christof.duhme@gmail.com>
2026-02-23 09:32:51 -0100

    Simplify.

--- install.sh
+++ install.sh
@@ -1,6 +1,2 @@
 #!/bin/bash 
-# Absolute path to this script, e.g. /home/user/bin/foo.sh
-SCRIPT=$(readlink -f "$0")
-# Absolute path this script is in, thus /home/user/bin
-SCRIPTPATH=$(dirname "$SCRIPT")
-stow -v -d $SCRIPTPATH -t ~ -R */
+stow -v -d . -t ~ -R */
