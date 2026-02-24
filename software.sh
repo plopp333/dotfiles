6aa574926e839341d68531350cee054790f6da0e
Christof Duhme <christof.duhme@gmail.com>
2026-02-24 14:47:48 +0100

    Add hypridle.

--- software.sh
+++ software.sh
@@ -3,7 +3,7 @@
 sudo pacman -S --noconfirm --needed alacritty zsh starship tmux nvim tree-sitter-cli less zoxide fzf jq direnv
 sudo pacman -S --noconfirm --needed wl-clipboard wtype cliphist
 sudo pacman -S --noconfirm --needed bitwarden-cli rofi-rbw
-sudo pacman -S --noconfirm --needed waybar rofi hyprlock brightnessctl
+sudo pacman -S --noconfirm --needed waybar rofi hyprlock hypridle brightnessctl
 sudo pacman -S --noconfirm --needed xdg-desktop-portal-gtk thunar thunar-volman gvfs adw-gtk-theme
 sudo pacman -S --noconfirm --needed ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols ttf-font-awesome
 
